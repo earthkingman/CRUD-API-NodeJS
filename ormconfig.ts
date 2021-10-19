e7a153641860c9f17b4d040810a68578cf2d3610
ji-park <qkrskdjf@naver.com>
2021-10-20 00:29:32 +0900

    feat(DB):Create entity

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export = {
+  type: "sqlite",
+  database: ":memory:",
+  synchronize: true,
+  logging: true,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
