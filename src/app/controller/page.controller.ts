c561e990ff866245c1fe5c67af35b557e8809c7e
ji-park <qkrskdjf@naver.com>
2021-10-24 03:51:47 +0900

    refactoring(jwt) : jwtutils -> Class

--- src/app/controller/page.controller.ts
+++ src/app/controller/page.controller.ts
@@ -5,7 +5,7 @@ import { PageService } from "../service/page.service"
 export class PageController {
     private pageService: PageService;
     public async page(req: Request, res: Response, next: NextFunction): Promise<any> {
-        this.pageService = new PageService
+        this.pageService = new PageService()
         const limit = Number(req.query.limit);
         const offset = Number(req.query.offset);
         const pageInfo = { limit, offset }
